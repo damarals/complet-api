a195e51ad34b2e676aa3301820de72a7d0bf511e
Daniel Amaral <silva.daniel86@gmail.com>
2023-11-20 20:08:55 -0300

    :sparkles: add api code

---
+++ app/src/api_v1/stems.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+from .utils import convert_to_mp3, download_youtube_video
+
+router = APIRouter()
+
+@router.get("/")
+async def list_stems():
+    results = {"Success": "All stems!"}
+    return results
+
+@router.post("/find/{id}")
+async def find_stem(stem_id: str):
+    results = {"Success": f"This is the stem with id: {stem_id}"}
+    return results
+
+@router.post("/generate")
+async def generate_stem(youtube_url: str):
+    video_filename = download_youtube_video(youtube_url)
+    audio_filename = convert_to_mp3(video_filename)
+
+    results = {"Success": f"Stems generated from YouTube video: {youtube_url}. Video file: {video_filename}. Audio file: {audio_filename}"}
+    return results
